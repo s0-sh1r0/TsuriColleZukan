e19645569bd917ff25f59db7cc080f9d4bb1e0a4
s0-sh1r0 <231205030@ccmailg.meijo-u.ac.jp>
2026-02-22 17:28:07 +0900

    Initial Commit

---
+++ TsuriColleZukanTests/TsuriColleZukanTests.swift
@@ -0,0 +1,17 @@
+//
+//  TsuriColleZukanTests.swift
+//  TsuriColleZukanTests
+//
+//  Created by 牛田壮志郎 on 2026/02/22.
+//
+
+import Testing
+@testable import TsuriColleZukan
+
+struct TsuriColleZukanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
