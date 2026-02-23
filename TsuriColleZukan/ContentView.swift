5c5da7f287975a500ef15416c4f5375852d139ee
s0-sh1r0 <146418104+s0-sh1r0@users.noreply.github.com>
2026-02-23 20:36:00 +0900

    Revert "RootViewにTCAを導入"

---
+++ TsuriColleZukan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TsuriColleZukan
+//
+//  Created by 牛田壮志郎 on 2026/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TsuriColleZukan/TsuriColleZukanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TsuriColleZukanApp.swift
+//  TsuriColleZukan
+//
+//  Created by 牛田壮志郎 on 2026/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TsuriColleZukanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
